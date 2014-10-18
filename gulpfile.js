6129351a261bb78c21b94207c130b2fc4bc890af
boxlabs <boxlabs.studios@gmail.com>
2014-10-18 20:37:31 -0300

    adding gulp deploy task to get the whole app deployed

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+// var merge = require('merge-stream');
+var rimraf = require('rimraf');
+var package = require('./package.json');
+
+// var path = require('path');
+var runSequence = require('run-sequence');
+var shelljs = require('shelljs');
+
+
+// General Config:
+var config = {
+  deploy: 'deploy',
+  clientDist: 'client/dist',
+  clientDest: 'deploy/public'
+};
+
+
+/***** Task: Build Client *****/
+gulp.task('build-client', function() {
+  return shelljs.exec('cd ./client && gulp build && cd ../');
+});
+
+/***** Task: Copy Client *****/
+gulp.task('copy-client', ['build-client'], function() {
+  return gulp
+    .src([config.clientDist + '/**/*.*'])
+    .pipe(gulp.dest(config.clientDest));
+});
+
+/***** Task: Copy Server *****/
+gulp.task('copy-server', function() {
+  return gulp
+    .src(['server/**/*'])
+    .pipe(gulp.dest(config.deploy));
+});
+
+/***** Task: Clean *****/
+gulp.task('clean', function(done) {
+  return rimraf(config.deploy, done);
+});
+
+/***** Task: Build *****/
+gulp.task('deploy', function(cbk) {
+    return runSequence('clean', [
+      'copy-client',
+      'copy-server'
+    ], cbk);
+  }
+);
+
+/***** Task: Default *****/
+gulp.task('default', [
+  'deploy'
+]);
--- server/models/element.js
+++ server/models/element.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
-    
+
 /**
  * Element Schema
  */
