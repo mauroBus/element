93338815c8c90317ff83e3728515aa067346f434
boxlabs <boxlabs.studios@gmail.com>
2015-01-24 12:57:38 -0300

    improving the swap of images in list after d&d

--- client/src/common/directives/file-upload/file-upload.controller.js
+++ client/src/common/directives/file-upload/file-upload.controller.js
@@ -19,38 +19,64 @@ angular.module('elementBoxApp.common')
 
   // CALLBACKS:
   uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
-    console.info('onWhenAddingFileFailed', item, filter, options);
+    // console.info('onWhenAddingFileFailed', item, filter, options);
   };
   uploader.onAfterAddingFile = function(fileItem) {
-    console.info('onAfterAddingFile', fileItem);
+    var index = uploader.queue.indexOf(fileItem);
+    fileItem.url = '/upload';
+    $scope.files[index] = { url: '', isUploaded: false };
+    // console.info('onAfterAddingFile', fileItem);
   };
   uploader.onAfterAddingAll = function(addedFileItems) {
-    console.info('onAfterAddingAll', addedFileItems);
+    // console.info('onAfterAddingAll', addedFileItems);
   };
   uploader.onBeforeUploadItem = function(item) {
-    console.info('onBeforeUploadItem', item);
+    // console.info('onBeforeUploadItem', item);
   };
   uploader.onProgressItem = function(fileItem, progress) {
-    console.info('onProgressItem', fileItem, progress);
+    // console.info('onProgressItem', fileItem, progress);
   };
   uploader.onProgressAll = function(progress) {
-    console.info('onProgressAll', progress);
+    // console.info('onProgressAll', progress);
   };
   uploader.onSuccessItem = function(fileItem, response, status, headers) {
-    console.info('onSuccessItem', fileItem, response, status, headers);
+    // console.info('onSuccessItem', fileItem, response, status, headers);
   };
   uploader.onErrorItem = function(fileItem, response, status, headers) {
-    console.info('onErrorItem', fileItem, response, status, headers);
+    // console.info('onErrorItem', fileItem, response, status, headers);
   };
   uploader.onCancelItem = function(fileItem, response, status, headers) {
-    console.info('onCancelItem', fileItem, response, status, headers);
+    // console.info('onCancelItem', fileItem, response, status, headers);
   };
   uploader.onCompleteItem = function(fileItem, response, status, headers) {
-    $scope.files.push({url: response.path});
-    console.info('onCompleteItem', fileItem, response, status, headers);
+    var index = uploader.queue.indexOf(fileItem);
+    $scope.files[index].url = response.path;
+    $scope.files[index].isUploaded = true;
+    // $scope.files.push({url: response.path});
+    // console.info('onCompleteItem', fileItem, response, status, headers);
   };
   uploader.onCompleteAll = function() {
-    console.info('onCompleteAll');
+    // console.info('onCompleteAll');
+  };
+
+  var swap = function(array, from, to) {
+    // array.splice(to, 0, array.splice(from, 1)[0]);
+    var elem = array[from];
+    array.splice(from, 1);
+    array.splice(to, 0, elem);
+  };
+
+  $scope.swapQueue = function(from, to) {
+    console.log(from);
+    console.log(to);
+    swap(uploader.queue, from, to);
+    swap($scope.files, from, to);
+  };
+
+  $scope.removeItem = function(item, index) {
+    item.remove();
+    // uploader.queue.splice(index, 1);
+    $scope.files.splice(index, 1);
   };
 
 }]);
