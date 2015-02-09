cae93e6825a27b04f2d54f00dce66b3166bed899
boxlabs <boxlabs.studios@gmail.com>
2015-02-09 12:32:17 -0300

    improving product view and info

--- client/src/app/products/product-new/product-new.controller.js
+++ client/src/app/products/product-new/product-new.controller.js
@@ -4,7 +4,15 @@ angular.module('elementBoxApp.products.productNew')
 .controller('ProductNewCtrl', [
           '$scope', '$stateParams', 'ProductsService', 'Categories',
   function($scope,   $stateParams,   ProductsService,   Categories) {
-    $scope.categories = Categories.getCategories();
+    // $scope.categories = Categories.getCategories();
+
+    var CategoryRsr = Categories.getCategoriesTree();
+    CategoryRsr
+      .query({flat: true})
+      .$promise.then(function(res) {
+        $scope.categories = res;
+      });
+
     $scope.catalogs = [{
         name: 'Winter',
         filter: 'winter'
@@ -60,7 +68,7 @@ angular.module('elementBoxApp.products.productNew')
     $scope.save = function() {
       var categs = [], catags = [];
       $scope.product.categories.forEach(function(p, i) {
-        if (p) { categs.push({ name: $scope.categories[i].filter }); }
+        if (p) { categs.push($scope.categories[i]._id); }
       });
       $scope.product.catalogs.forEach(function(p, i) {
         if (p) { catags.push({ name: $scope.catalogs[i].filter }); }
