5a63b35b23b695bd4e20ba59b7d479da1ee998ff
boxlabs <boxlabs.studios@gmail.com>
2015-02-20 16:11:43 -0300

    improving product new module

--- client/src/app/products/product-new/product-new.controller.js
+++ client/src/app/products/product-new/product-new.controller.js
@@ -4,7 +4,15 @@ angular.module('elementBoxApp.products.productNew')
 .controller('ProductNewCtrl', [
           '$scope', '$stateParams', 'ProductsService', 'Categories',
   function($scope,   $stateParams,   ProductsService,   Categories) {
-    // $scope.categories = Categories.getCategories();
+    $scope.productCreated = false;
+    $scope.areImgsCollapsed = true;
+    $scope.product = {
+      title: '',
+      description: '',
+      images: [],
+      categories: [],
+      price: 0,
+    };
 
     var CategoryRsr = Categories.getCategoriesTree();
     CategoryRsr
@@ -13,69 +21,17 @@ angular.module('elementBoxApp.products.productNew')
         $scope.categories = res;
       });
 
-    $scope.catalogs = [{
-        name: 'Winter',
-        filter: 'winter'
-      }, {
-        name: 'Outum',
-        filter: 'outum'
-      }, {
-        name: 'Summer',
-        filter: 'summer'
-      }
-    ];
-
-    $scope.productCreated = false;
-
-    $scope.product = {
-      title: '',
-      description: '',
-      images: [],
-      categories: [],
-      style: Math.floor(Math.random()*10000),
-      // variants: [{
-      //     color: 'Black',
-      //     images: [{
-      //         kind: 'thumbnail',
-      //         url: 'imgs/products/1.png'
-      //       },
-      //       {
-      //         kind: 'catalog',
-      //         url: 'imgs/products/1.png'
-      //       }
-      //     ],
-      //     sizes: [{
-      //         size: 'S',
-      //         available: 10,
-      //         sku: 'CAT-1234-Blk-S',
-      //         price: 99.99
-      //       },
-      //       {
-      //         size: 'M',
-      //         available: 7,
-      //         sku: 'CAT-1234-Blk-M',
-      //         price: 109.99
-      //       }
-      //     ]
-      //   }],
-        catalogs: [
-          // { 'name': 'Apparel' }
-        ]
-    };
-
-    $scope.areImgsCollapsed = true;
-
     $scope.save = function() {
-      var categs = [], catags = [];
+      var categs = [];
       $scope.product.categories.forEach(function(p, i) {
         if (p) { categs.push($scope.categories[i]._id); }
       });
-      $scope.product.catalogs.forEach(function(p, i) {
-        if (p) { catags.push({ name: $scope.catalogs[i].filter }); }
-      });
+      // $scope.product.catalogs.forEach(function(p, i) {
+      //   if (p) { catags.push({ name: $scope.catalogs[i].filter }); }
+      // });
 
       ProductsService
-        .save(angular.extend({}, $scope.product, {categories: categs, catalogs: catags}), function() {
+        .save(angular.extend({}, $scope.product, {categories: categs}), function() {
           $scope.productCreated = true;
         });
     };
