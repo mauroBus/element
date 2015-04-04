5a1d3051de30bd22703f6dfc955a63b93b7abd1d
boxlabs <boxlabs.studios@gmail.com>
2015-04-04 20:46:30 -0300

    improving header with navigation pie/radial menu

--- client/src/app/main/header/header.controller.js
+++ client/src/app/main/header/header.controller.js
@@ -4,51 +4,62 @@ angular.module('elementBoxApp.main')
 .controller('HeaderCtrl', [
           '$scope', '$rootScope', '$state', '$stateParams', 'AuthService', 'AUTH_EVENTS', 'USER_ROLES',
   function($scope,  $rootScope,    $state,   $stateParams,   AuthService,   AUTH_EVENTS,   Roles) {
-    var scope = {
-      title: 'Element SandBox',
+    $scope.title = 'Element SandBox';
+    $scope.isMenuOpen = false;
+    $scope.Roles = Roles;
 
-      getSubStateName: function() {
-//         main.home
-// main.newelement
-// main.about
-// main.myadmin
-// main.products
-        if ($state.path() === '/home') {
-          return 'Home Page';
-        }
-        if ($state.path() === '/newelement') {
-          return 'New Element Page';
-        }
-        if ($state.path() === '/about') {
-          return 'About Page';
-        }
-        if ($stateParams.dni) {
-          return 'Element dni:' + $stateParams.dni + ' Info';
-        }
-      },
-
-      showLink: function(state, role) {
-        var regExp = new RegExp('^' + state);
-        if (role && (!$scope.currentUser || angular.equals({}, $scope.currentUser))) {
-          return false;
-        }
-        if (role && ($scope.currentUser.roles.indexOf(role) < 0)) {
-          return false;
-        }
-        // $scope.subStateName = $scope.getSubStateName();
-        return !regExp.test($state.current.name);
-      },
-
-      signout: function() {
-        AuthService.signout($scope.credentials)
-          .then(function(res) {
-            $rootScope.$broadcast(AUTH_EVENTS.signoutSuccess, res.data.user);
-          });
-      },
-
-      Roles: Roles
+    $scope.getSubStateName = function() {
+      // main.home
+      // main.newelement
+      // main.about
+      // main.myadmin
+      // main.products
+      if ($state.path() === '/home') {
+        return 'Home Page';
+      }
+      if ($state.path() === '/newelement') {
+        return 'New Element Page';
+      }
+      if ($state.path() === '/about') {
+        return 'About Page';
+      }
+      if ($stateParams.dni) {
+        return 'Element dni:' + $stateParams.dni + ' Info';
+      }
     };
 
-    angular.extend($scope, scope);
+    $scope.showLink = function(state, role) {
+      if (role && (!$scope.currentUser || angular.equals({}, $scope.currentUser))) {
+        return false;
+      }
+      if (role && ($scope.currentUser.roles.indexOf(role) < 0)) {
+        return false;
+      }
+      // $scope.subStateName = $scope.getSubStateName();
+      return true;
+    };
+
+    $scope.isActive = function(state) {
+      var regExp = new RegExp('^' + state);
+      return regExp.test($state.current.name);
+    };
+
+    $scope.signout = function() {
+      AuthService.signout($scope.credentials)
+        .then(function(res) {
+          $rootScope.$broadcast(AUTH_EVENTS.signoutSuccess, res.data.user);
+        });
+    };
+
+    $scope.toggleMenu = function($event) {
+      $scope.isMenuOpen = !$scope.isMenuOpen;
+    };
+
+    $scope.goto = function(state) {
+      if ($state.current.name !== state) {
+        $state.go(state);
+      }
+    };
+
   }
 ]);
