dbcae7605e05be6d0bc245f64268ff8fee985927
boxlabs <boxlabs.studios@gmail.com>
2015-04-26 18:23:06 -0300

    refactoring heaer

--- client/src/app/main/header/header.controller.js
+++ client/src/app/main/header/header.controller.js
@@ -2,47 +2,30 @@
 angular.module('elementBoxApp.main')
 
 .controller('HeaderCtrl', [
-          '$scope', '$rootScope', '$state', '$stateParams', 'AuthService', 'AUTH_EVENTS', 'USER_ROLES',
-  function($scope,  $rootScope,    $state,   $stateParams,   AuthService,   AUTH_EVENTS,   Roles) {
+          '$scope', '$rootScope', '$state', '$stateParams', 'AuthService', 'AUTH_EVENTS',
+  function($scope,  $rootScope,    $state,   $stateParams,   AuthService,   AUTH_EVENTS) {
     $scope.title = 'Element SandBox';
-    $scope.isMenuOpen = false;
-    $scope.Roles = Roles;
+    // $scope.isMenuOpen = false;
 
-    $scope.getSubStateName = function() {
-      // main.home
-      // main.newelement
-      // main.about
-      // main.myadmin
-      // main.products
-      if ($state.path() === '/home') {
-        return 'Home Page';
-      }
-      if ($state.path() === '/newelement') {
-        return 'New Element Page';
-      }
-      if ($state.path() === '/about') {
-        return 'About Page';
-      }
-      if ($stateParams.dni) {
-        return 'Element dni:' + $stateParams.dni + ' Info';
-      }
-    };
-
-    $scope.showLink = function(state, role) {
-      if (role && (!$scope.currentUser || angular.equals({}, $scope.currentUser))) {
-        return false;
-      }
-      if (role && ($scope.currentUser.roles.indexOf(role) < 0)) {
-        return false;
-      }
-      // $scope.subStateName = $scope.getSubStateName();
-      return true;
-    };
-
-    $scope.isActive = function(state) {
-      var regExp = new RegExp('^' + state);
-      return regExp.test($state.current.name);
-    };
+    // $scope.getSubStateName = function() {
+    //   // main.home
+    //   // main.newelement
+    //   // main.about
+    //   // main.myadmin
+    //   // main.products
+    //   if ($state.path() === '/home') {
+    //     return 'Home Page';
+    //   }
+    //   if ($state.path() === '/newelement') {
+    //     return 'New Element Page';
+    //   }
+    //   if ($state.path() === '/about') {
+    //     return 'About Page';
+    //   }
+    //   if ($stateParams.dni) {
+    //     return 'Element dni:' + $stateParams.dni + ' Info';
+    //   }
+    // };
 
     $scope.signout = function() {
       AuthService.signout($scope.credentials)
@@ -51,15 +34,9 @@ angular.module('elementBoxApp.main')
         });
     };
 
-    $scope.toggleMenu = function($event) {
-      $scope.isMenuOpen = !$scope.isMenuOpen;
-    };
-
-    $scope.goto = function(state) {
-      if ($state.current.name !== state) {
-        $state.go(state);
-      }
-    };
+    // $scope.toggleMenu = function($event) {
+    //   $scope.isMenuOpen = !$scope.isMenuOpen;
+    // };
 
   }
 ]);
