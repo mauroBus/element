5f86b789ea8340b3926607776bee550ac99acef1
boxlabs <boxlabs.studios@gmail.com>
2015-04-26 18:24:05 -0300

    refactoring in main to allow separated menu section

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -1,8 +1,15 @@
 angular.module('elementBoxApp.main')
 
-.controller('MainCtrl', ['$state', function($state) {
+.controller('MainCtrl', ['$state', '$scope', function($state, $scope) {
     if ($state.current.name === 'main') {
       $state.go('main.home');
     }
+
+    $scope.menuOpened = false;
+
+    $scope.toggleMenu = function() {
+      $scope.menuOpened = !$scope.menuOpened;
+    };
+
   }
 ]);
