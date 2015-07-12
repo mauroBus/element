fc81a2ab0cd60753370a3b71f281c6c02f8c3957
boxlabs <boxlabs.studios@gmail.com>
2015-07-12 00:09:39 -0300

    product new ui refactor - select categ...

--- client/src/app/products/product-new/product-new.controller.js
+++ client/src/app/products/product-new/product-new.controller.js
@@ -2,10 +2,15 @@
 angular.module('elementBoxApp.products.productNew')
 
 .controller('ProductNewCtrl', [
-          '$scope', '$stateParams', 'ProductsService', 'Categories',
-  function($scope,   $stateParams,   ProductsService,   Categories) {
+          '$scope', '$rootScope', '$stateParams', 'ProductsService', 'Categories',
+  function($scope,   $rootScope,   $stateParams,   ProductsService,   Categories) {
     $scope.productCreated = false;
     $scope.areImgsCollapsed = true;
+    $scope.isCategSelected = false;
+    $scope.selectedCateg = null;
+
+    $rootScope.$emit('title', 'Create a Product');
+
     $scope.product = {
       title: '',
       description: '',
@@ -16,16 +21,18 @@ angular.module('elementBoxApp.products.productNew')
 
     var CategoryRsr = Categories.getCategoriesTree();
     CategoryRsr
-      .query({flat: true})
+      // .query({flat: true})
+      .query({ flat: false })
       .$promise.then(function(res) {
         $scope.categories = res;
       });
 
     $scope.save = function() {
       var categs = [];
-      $scope.product.categories.forEach(function(p, i) {
-        if (p) { categs.push($scope.categories[i]._id); }
-      });
+      // $scope.product.categories.forEach(function(p, i) {
+        // if (p) { categs.push($scope.categories[i]._id); }
+      // });
+
       // $scope.product.catalogs.forEach(function(p, i) {
       //   if (p) { catags.push({ name: $scope.catalogs[i].filter }); }
       // });
@@ -35,5 +42,16 @@ angular.module('elementBoxApp.products.productNew')
           $scope.productCreated = newProduct;
         });
     };
+
+    $scope.onCategSelected = function(path, categName) {
+      $scope.isCategSelected = true;
+      $scope.selectedCateg = path[path.length-1];
+    };
+
+    $scope.onCategUnselected = function(path, categName) {
+      $scope.isCategSelected = false;
+      $scope.selectedCateg = null;
+    };
+
   }
 ]);
