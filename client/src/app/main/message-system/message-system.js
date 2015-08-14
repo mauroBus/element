c29cd704deb60a446b2011535148413d8b5fa8e8
boxlabs <boxlabs.studios@gmail.com>
2015-08-14 12:18:32 -0300

    adding message-system module

---
+++ client/src/app/main/message-system/message-system.js
@@ -0,0 +1,38 @@
+
+angular.module('elementBoxApp.main')
+
+.directive('messageSystem', [
+  function() {
+    return {
+      restrict: 'EA',
+      scope: {},
+      templateUrl: 'main/message-system/message-system.html',
+      controller: ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout) {
+
+        $scope.msg = {};
+        $scope.displayMsg = false;
+
+        var init = function() {
+          $rootScope.$on('app-msg', function(ev, msg) {
+            $timeout(function() {
+              $scope.msg = msg;
+              $scope.displayMsg = true;
+            });
+          });
+        };
+
+        $scope.close = function() {
+          $timeout(function() {
+            $scope.displayMsg = false;
+          });
+          $timeout(function() {
+            $scope.msg = {};
+          }, 2000);
+        };
+
+        init();
+      }]
+    };
+
+  }
+]);
