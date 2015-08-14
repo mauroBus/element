e15c63d3496e7adeb2038ec3595cc4ae1ea06a37
boxlabs <boxlabs.studios@gmail.com>
2015-08-14 12:10:58 -0300

    refactoring/improving product-edit module

--- client/src/app/products/product-edit/product-edit.controller.js
+++ client/src/app/products/product-edit/product-edit.controller.js
@@ -2,12 +2,16 @@
 angular.module('elementBoxApp.products.productEdit')
 
 .controller('ProductEditCtrl', [
-          '$scope', '$rootScope', '$stateParams', 'ProductsService', 'Categories',
-  function($scope,   $rootScope,   $stateParams,   ProductsService,   Categories) {
+          '$scope', '$rootScope', '$stateParams', 'ProductsService', 'Categories', 'ProdErrorsService',
+  function($scope,   $rootScope,   $stateParams,   ProductsService,   Categories,   ProdErrorsService) {
     $scope.slides = [];
     $scope.slidesIndex = 0;
     $scope.categories = [];
     $scope.productSaved = false;
+    $scope.isCategSelected = false;
+    $scope.selectedCateg = null;
+    $scope.error = {};
+    // $scope.areImgsCollapsed = false;
 
     $rootScope.$emit('title', 'Edit Product');
 
@@ -20,7 +24,7 @@ angular.module('elementBoxApp.products.productEdit')
 
     var CategoryRsr = Categories.getCategoriesTree();
     CategoryRsr
-      .query({flat: true})
+      .query({ flat: false })
       .$promise.then(function(res) {
         $scope.categories = res;
       });
@@ -36,18 +40,33 @@ angular.module('elementBoxApp.products.productEdit')
 
     $scope.save = function() {
       var categs = [], catags = [];
-      $scope.product.categories.forEach(function(p, i) {
-        if (p) { categs.push($scope.categories[i]._id); }
-      });
-      // $scope.product.catalogs.forEach(function(p, i) {
-      //   if (p) { catags.push({ name: $scope.catalogs[i].filter }); }
-      // });
 
-      angular.extend($scope.product, { categories: categs, catalogs: catags });
+      ProdErrorsService.checkProdErrors($scope.error, $scope.product, $scope.selectedCateg);
+
+      if ($scope.error.category || $scope.error.title || $scope.error.price || $scope.error.description || $scope.error.images) {
+        // $('.field-set__item--category').scrollIntoView();
+        window.scrollTo(0, 150);
+        return;
+      }
+
+      var data = angular.extend({}, $scope.product, {
+        categories: [ $scope.selectedCateg._id ]
+      });
 
       $scope.product.$update(function() {
         $scope.productSaved = true;
       });
+    };
+
+    $scope.onCategSelected = function(path, categName) {
+      $scope.isCategSelected = true;
+      $scope.selectedCateg = path[path.length-1];
+      $scope.error.category = false;
+    };
+
+    $scope.onCategUnselected = function(path, categName) {
+      $scope.isCategSelected = false;
+      $scope.selectedCateg = null;
     };
 
   }
