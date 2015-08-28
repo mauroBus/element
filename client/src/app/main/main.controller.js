1ae88327b336365e7f77255c32bbd580fe31ee36
boxlabs <boxlabs.studios@gmail.com>
2015-08-28 13:16:11 -0300

    improving main module

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -1,6 +1,6 @@
 angular.module('elementBoxApp.main')
 
-.controller('MainCtrl', ['$state', '$scope', '$rootScope', function($state, $scope, $rootScope) {
+.controller('MainCtrl', ['$state', '$scope', '$rootScope', '$timeout', function($state, $scope, $rootScope, $timeout) {
     if ($state.current.name === 'main') {
       $state.go('main.home');
     }
@@ -9,13 +9,22 @@ angular.module('elementBoxApp.main')
     $scope.defaultTitle = 'SandBox App';
 
     $scope.menuOpened = false;
+    var $title = window.$('head title');
 
     $scope.toggleMenu = function() {
       $scope.menuOpened = !$scope.menuOpened;
     };
 
     $rootScope.$on('title', function(event, title) {
+      if (!title) { return; }
       $scope.appTitle = title;
+
+      $timeout(function() { // waiting 500ms for updating the router url before the page title (browser nav issue).
+        $title.text($scope.defaultTitle + ' - ' + title);
+      }, 500);
+    });
+
+    $rootScope.$on('title', function(event, title) {
     });
 
   }
