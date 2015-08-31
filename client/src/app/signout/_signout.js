d92a578d2de0254151d0a0e2242be46cebd8d7da
boxlabs <boxlabs.studios@gmail.com>
2015-08-31 11:34:02 -0300

    signout improvements

--- client/src/app/signout/_signout.js
+++ client/src/app/signout/_signout.js
@@ -5,16 +5,64 @@ angular.module('elementBoxApp.signout', [])
     $stateProvider.state('main.signout', {
       url: '/signout',
       templateUrl: 'signout/signout.html',
-      controller: ['$rootScope', '$state', 'Session', function($rootScope, $state, Session) {
-        $rootScope.$emit('title', 'Sign Out');
-
-        if (Session.getSession() && Session.getSession().user) {
-          $state.go('main.home');
-          return;
-        }
-
-        $rootScope.$emit('app-msg', { txt: 'You are logged out!' });
-      }]
+      controller: 'signoutCtrl'
     });
   }
-]);
+])
+
+.controller('signoutCtrl', [
+        '$scope', '$rootScope', '$state', 'Session',
+function($scope,   $rootScope,   $state,   Session) {
+  $rootScope.$emit('title', 'Sign Out');
+
+  if (Session.getSession() && Session.getSession().user) {
+    $state.go('main.home');
+    return;
+  }
+
+  var mouseBinding;
+
+  $rootScope.$emit('app-msg', { txt: 'You are logged out!' });
+
+  var mouse = function() {
+    var $scene = window.$('.scene'),
+        sceneAtts = {},
+        $magic = window.$('.magic'),
+        magicWHalf = $magic.width() / 2;
+
+    sceneAtts = $scene.offset();
+    sceneAtts.w = $scene.outerWidth();
+    sceneAtts.h = $scene.outerHeight();
+
+    mouseBinding = window.$(document).on('mousemove', function(e) {
+      if (
+          e.pageX < sceneAtts.left || e.pageX > sceneAtts.left + sceneAtts.w ||
+          e.pageY < sceneAtts.top || e.pageY > sceneAtts.top + sceneAtts.h
+        ) {
+        $magic.css({
+          left: 'calc(50% - 10rem)',
+          top: 'calc(50% - 10rem)'
+        });
+        return;
+      }
+
+      $magic.css({
+        left: e.pageX - magicWHalf,
+        top: e.pageY - magicWHalf * 2 - 40,
+        // left: e.offsetX - magicWHalf - 3,
+        // top: e.offsetY - magicWHalf * 2 - 30,
+        display: ''
+      });
+      // $magic.clientX = e.clientX - magicWHalf;
+      // $magic.clientY = e.clientY - magicWHalf;
+    });
+  };
+
+  setTimeout(mouse, 1500);
+
+  // $scope.$on('destroy', function() {
+  //   if (mouseBinding) {
+  //     mouseBinding();
+  //   }
+  // });
+}]);
