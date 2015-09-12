6c98554c7a5909e71ab47c098e2eebffa756fdff
boxlabs <boxlabs.studios@gmail.com>
2015-09-12 18:40:25 -0300

    signup improvements

--- client/src/app/signup/signup.controller.js
+++ client/src/app/signup/signup.controller.js
@@ -10,9 +10,10 @@ angular.module('elementBoxApp.signup')
       return;
     }
 
-    $rootScope.$emit('title', 'Sign Up');
+    $rootScope.$emit('title', 'TITLE_SIGN_UP');
 
     $scope.displayError = false;
+    $scope.inProgress = false;
     $scope.errors = {};
 
     $scope.credentials = {
@@ -20,7 +21,7 @@ angular.module('elementBoxApp.signup')
       lastName: '',
       email: '',
       password: '',
-      displayName: '',
+      // displayName: '',
       username: ''
     };
 
@@ -29,14 +30,20 @@ angular.module('elementBoxApp.signup')
         $scope.displayError = true;
         return;
       }
+
+      $scope.displayError = false;
+      $scope.inProgress = true;
+      $scope.errors = {};
+
       AuthService.signup($scope.credentials)
         .then(function(res) { // success cbk.
           $rootScope.$broadcast(AUTH_EVENTS.singinSuccess, {user: res.data.user, navigate: true});
-          $scope.displayError = false;
+          $scope.inProgress = false;
         }, function(res) { // error cbk.
           // $rootScope.$broadcast(AUTH_EVENTS.singinFailed);
-          console.log(res.data.errors);
           angular.extend($scope.errors, res.data.errors || {});
+          $scope.displayError = true;
+          $scope.inProgress = false;
         });
     };
   }
